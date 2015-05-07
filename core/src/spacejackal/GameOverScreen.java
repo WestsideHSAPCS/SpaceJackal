9325274cfa52f34c59c6dd8509a74cc8cfe6f90e
ryanstejskal <stejskal.ryan@westside66.net>
2015-05-07 14:13:07 -0500

    Created start of game-over screen.

---
+++ core/src/spacejackal/GameOverScreen.java
@@ -0,0 +1,27 @@
+package spacejackal;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameOverScreen implements Screen
+{
+	@Override
+	public void init(SpaceJackalGame game)
+	{
+	}
+
+	@Override
+	public void update(SpaceJackalGame game)
+	{
+	}
+
+	@Override
+	public void render(SpriteBatch sBatch)
+	{
+	}
+
+	@Override
+	public Screen getNextScreen()
+	{
+		return null;
+	}
+}
